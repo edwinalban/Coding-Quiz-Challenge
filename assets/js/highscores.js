f389d377e21a6c1a3008ecd92e7777942e5b5124
Yachtzee <edwinalban@gmail.com>
2023-05-20 12:42:22 -0700

    added array with questions as objects to quiz questions js

---
+++ assets/js/highscores.js
@@ -0,0 +1,2 @@
+// on submit button click add new final score to ol
+
---
+++ assets/js/quiz-questions.js
@@ -0,0 +1,58 @@
+var questionsArr = [
+    
+    {question1: "Commonly used data types DO NOT include:",
+    answerIndex: 2,
+    choices: [
+        "Strings",
+        "Booleans",
+        "Alerts",
+        "Numbers"
+    ]
+},
+    {question2: "The condition in an 'if/else' statement is enclosed within ___.",
+    answerIndex: 2,
+    choices: [
+        "Quotes",
+        "Bracers",
+        "Parentheses",
+        "Brackets"
+    ]
+},
+    {question3: "Arrays in Javascript can be used to store ____.",
+    answerIndex: 3,
+    choices: [
+        "Numbers and Strings",
+        "Other arrays",
+        "Booleans",
+        "All of the above",
+    ]
+},
+    {question4: "String values must be enclosed within ____ when being assigned to variables.",
+    answerIndex: 3,
+    choices: [
+        "Commas",
+        "Bracers",
+        "Parentheses",
+        "Quotation Marks"
+    ]
+},
+
+    {question5: "A very useful tool used during development and debugging for printing content to the debugger is ____.",
+    answerIndex: 3,
+    choices: [
+        "JavaScript",
+        "Terminal/Bash",
+        "For loops",
+        "Console.log"
+    ]
+}
+];
+
+var startButton = document.getElementById("start");
+
+startButton.addEventListener("Click", function(event) {
+    event.preventDefault();
+
+    localStorage.setItem("questionsArr", JSON.stringify(question1));
+    console.log(localStorage.question1);
+    });
